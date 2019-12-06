6a194d337c5d20741a7fa0a57d08e5b9f980a890
David Cantoran <davidcantoran36@gmail.com>
2019-12-06 22:49:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -85,6 +86,7 @@ def adding_matz
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
 }
+  binding.pry
   programmer_hash
 end
 
